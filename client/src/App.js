2c7a0745295b2214a1e2e28030a10502eba4b92e
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-30 15:26:49 +1200

    feat(client): add InputToDo component to App, with a styling div container

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import { Fragment } from 'react'
+
 import './App.css'
+import InputToDo from './components/InputToDo'
 
-function App() {
-  return <Fragment></Fragment>
+export default function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputToDo />
+      </div>
+    </Fragment>
+  )
 }
-
-export default App

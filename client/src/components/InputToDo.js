29ce6d21082f75f2020d3596a26191f3daabe174
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-30 15:38:20 +1200

    feat(client): add working functionality for InputToDo form

--- client/src/components/InputToDo.js
+++ client/src/components/InputToDo.js
@@ -1,11 +1,33 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 
 export default function InputToDo() {
+  const [description, setDescription] = useState('')
+
+  async function onSubmitForm(e) {
+    e.preventDefault()
+    try {
+      const body = { description }
+      const response = await fetch('http://localhost:4000/todos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      console.log(response)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Pern To Do</h1>
-      <form className="d-flex mt-5">
-        <input type="text" className="form-control" />
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button className="btn btn-success">Add</button>
       </form>
     </Fragment>

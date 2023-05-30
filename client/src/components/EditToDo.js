128fa19faf3bbcc4aceee02cc9cf84a80a397e2b
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-30 16:48:04 +1200

    feat(client): add functionality to the EditTodo.js component

--- client/src/components/EditToDo.js
+++ client/src/components/EditToDo.js
@@ -1,40 +1,71 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 
 export default function EditToDo({ todo }) {
+  const [description, setDescription] = useState(todo.description)
+
+  async function updateDescripion(e) {
+    e.preventDefault()
+    try {
+      const body = { description }
+      const response = await fetch(
+        `http://localhost:4000/todos/${todo.todo_id}`,
+        {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        }
+      )
+      window.location = '/'
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <Fragment>
       <button
         type="button"
         className="btn btn-warning"
         data-toggle="modal"
-        data-target="#myModal"
+        data-target={`#id${todo.todo_id}`}
       >
         Edit
       </button>
 
-      {/* <!-- The Modal --> */}
-      <div className="modal" id="myModal">
+      <div
+        className="modal"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
         <div className="modal-dialog">
           <div className="modal-content">
-            {/* <!-- Modal Header --> */}
             <div className="modal-header">
               <h4 className="modal-title">Edit Todo</h4>
-              <button type="button" className="close" data-dismiss="modal">
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
                 &times;
               </button>
             </div>
 
-            {/* <!-- Modal body --> */}
             <div className="modal-body">
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
             </div>
 
-            {/* <!-- Modal footer --> */}
             <div className="modal-footer">
               <button
                 type="button"
                 className="btn btn-warning"
                 data-dismiss="modal"
+                onClick={(e) => updateDescripion(e)}
               >
                 Edit
               </button>
@@ -42,6 +73,7 @@ export default function EditToDo({ todo }) {
                 type="button"
                 className="btn btn-danger"
                 data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
               >
                 Close
               </button>

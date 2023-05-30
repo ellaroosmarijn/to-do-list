9d511f963b46b16c3abef099ae5599fcdc92289f
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-30 16:10:22 +1200

    feat(client): add functionality logic for listing all of the todos and deleteing them

--- client/src/components/ListToDo.js
+++ client/src/components/ListToDo.js
@@ -1,6 +1,34 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 
 export default function ListToDos() {
+  const [todos, setTodos] = useState([])
+
+  async function deleteTodo(id) {
+    try {
+      const deleteToDo = await fetch(`http://localhost:4000/todos/${id}`, {
+        method: 'DELETE',
+      })
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id))
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  async function getToDos() {
+    try {
+      const response = await fetch('http://localhost:4000/todos')
+      const jsonData = await response.json()
+      setTodos(jsonData)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getToDos()
+  }, [])
+
   return (
     <Fragment>
       <table class="table mt-5 text-center">
@@ -11,7 +39,27 @@ export default function ListToDos() {
             <th>Delete</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {/* <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </Fragment>
   )

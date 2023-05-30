81abfd3d1b55ab0afce71090f973b950d7b0752d
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-30 16:09:32 +1200

    feat(client): add the ListTodo.js component to the App

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { Fragment } from 'react'
 
 import './App.css'
 import InputToDo from './components/InputToDo'
+import ListToDos from './components/ListToDo'
 
 export default function App() {
   return (
     <Fragment>
       <div className="container">
         <InputToDo />
+        <ListToDos />
       </div>
     </Fragment>
   )
